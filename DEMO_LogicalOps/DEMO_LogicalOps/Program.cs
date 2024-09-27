fde5dafff37abab7c306db6902ae858fd3ea0330
Weisberg Barros <weisbergbsf@gmail.com>
2024-09-27 11:15:43 -0300

    finished demo

---
+++ DEMO_LogicalOps/DEMO_LogicalOps/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace DEMO_LogicalOps
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("What is your annual income?: ");
+            //double annualIncome = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("How many years have you been at your current job?: ");
+            //double years = Convert.ToDouble(Console.ReadLine());
+
+            //if (annualIncome >= 45000 && years > 3)
+            //{
+            //    Console.WriteLine("Congrats! You qualify for the loan.");
+            //    Console.WriteLine("Congrats! You qualify for the loan.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Sorry, you do not qualify for the loan");
+            //}
+
+            //Console.Write("Enter your age: ");
+            //int age = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Are you a member? (yes/no): ");
+            //string membership = Console.ReadLine();
+
+            //if (membership == "yes" || age >= 65)
+            //{
+            //    Console.WriteLine("You are eligible for the discount");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You are not eligible for the discount");
+            //}
+        }
+    }
+}
